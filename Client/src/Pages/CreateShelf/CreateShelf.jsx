2a81d628bbf2293884f6f30b922ebfb288d214a8
Anna N <nahid.najafi.h@gmail.com>
2024-07-23 11:03:46 +0200

    update 11:03

--- Client/src/Pages/CreateShelf/CreateShelf.jsx
+++ Client/src/Pages/CreateShelf/CreateShelf.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";  //to show notifications
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import TimePicker from "react-time-picker";
 import "react-time-picker/dist/TimePicker.css";
 import "react-clock/dist/Clock.css";
-import "./CustomTimePicker.css"; 
-import CountryStateCitySelector from "../../Components/CreateShelfComponent/CountryStateCitySelector.jsx";
+import "./CustomTimePicker.css";
+import CountryStateCitySelector from "../../Components/CreateShelfComponent/CountryStateCitySelector";
 
 
 const CreateShelfForm = () => {
@@ -15,9 +16,15 @@ const CreateShelfForm = () => {
     name: "",
     openingTime: "",
     closingTime: "",
+    country: "",
+    state: "",
+    city: "",
+    street: "",
+    postalCode: "",
   });
 
   const [images, setImages] = useState([]);
+  const [is24Hours, setIs24Hours] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -35,13 +42,13 @@ const CreateShelfForm = () => {
   const handleLocationChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData();
     data.append("name", formData.name);
-    data.append("openingTime", formData.openingTime);
-    data.append("closingTime", formData.closingTime);
+    data.append("openingTime", is24Hours ? "00:00" : formData.openingTime);
+    data.append("closingTime", is24Hours ? "23:59" : formData.closingTime);
     data.append("country", formData.country);
     data.append("state", formData.state);
     data.append("city", formData.city);
@@ -67,6 +74,10 @@ const CreateShelfForm = () => {
     } catch (error) {
       toast.error(error.response?.data?.message || "Error creating bookshelf");
     }
+  };
+
+  const handleRadioChange = (e) => {
+    setIs24Hours(e.target.value === "24hours");
   };
 
   return (
@@ -119,24 +130,6 @@ const CreateShelfForm = () => {
 
       <div className="mb-4">
         <label
-          htmlFor="street"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Street:
-        </label>
-        <input
-          type="text"
-          id="street"
-          name="street"
-          value={formData.street}
-          onChange={handleChange}
-          required
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label
           htmlFor="postalCode"
           className="block text-sm font-medium text-gray-700"
         >
@@ -155,37 +148,89 @@ const CreateShelfForm = () => {
 
       <div className="mb-4">
         <label
-          htmlFor="openingTime"
+          htmlFor="street"
           className="block text-sm font-medium text-gray-700"
         >
-          Opening Time:
+          Street:
         </label>
-        <TimePicker
-          id="openingTime"
-          name="openingTime"
-          value={formData.openingTime}
-          onChange={(value) => handleTimeChange("openingTime", value)}
+        <input
+          type="text"
+          id="street"
+          name="street"
+          value={formData.street}
+          onChange={handleChange}
           required
-          className="time-picker-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         />
       </div>
 
       <div className="mb-4">
-        <label
-          htmlFor="closingTime"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Closing Time:
+        <label className="block text-sm font-medium text-gray-700">
+          Opening Time:
         </label>
-        <TimePicker
-          id="closingTime"
-          name="closingTime"
-          value={formData.closingTime}
-          onChange={(value) => handleTimeChange("closingTime", value)}
-          required
-          className="time-picker-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
+        <div className="flex items-center">
+          <input
+            type="radio"
+            id="24hours"
+            name="timeOption"
+            value="24hours"
+            checked={is24Hours}
+            onChange={handleRadioChange}
+            className="mr-2"
+          />
+          <label htmlFor="24hours" className="mr-4">
+            24 Hours
+          </label>
+          <input
+            type="radio"
+            id="customTime"
+            name="timeOption"
+            value="customTime"
+            checked={!is24Hours}
+            onChange={handleRadioChange}
+            className="mr-2"
+          />
+          <label htmlFor="customTime">Custom Time</label>
+        </div>
       </div>
+
+      {!is24Hours && (
+        <>
+          <div className="mb-4">
+            <label
+              htmlFor="openingTime"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Opening Time:
+            </label>
+            <TimePicker
+              id="openingTime"
+              name="openingTime"
+              value={formData.openingTime}
+              onChange={(value) => handleTimeChange("openingTime", value)}
+              required
+              className="time-picker-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="closingTime"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Closing Time:
+            </label>
+            <TimePicker
+              id="closingTime"
+              name="closingTime"
+              value={formData.closingTime}
+              onChange={(value) => handleTimeChange("closingTime", value)}
+              required
+              className="time-picker-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </>
+      )}
 
       <button
         type="submit"
@@ -196,4 +241,5 @@ const CreateShelfForm = () => {
     </form>
   );
 };
+
 export default CreateShelfForm;
